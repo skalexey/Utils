c8bed849b33009660c8bc43ea7e183bb171aeeb7
skalexey <skorohodov-ad@narod.ru>
2022-05-01 13:15:22 +0300

    Log.h debug level fixes

--- Log.h
+++ Log.h
@@ -9,7 +9,7 @@
 	#define SET_LOG_VERBOSE(boolVal) namespace { bool verboseLevel = boolVal;}
 	#define SET_LOG_DEBUG(boolVal) namespace { bool debugLevel = boolVal; }
 	#define LOCAL_LOG(msg) logStream() << "[" << logTitle << "] " << msg
-	#define LOCAL_DEBUG(msg) { if (debugLevel) { logStream() << "[" << logTitle << "] [D]" << msg << "\n"; } };
+	#define LOCAL_DEBUG(msg) { if (debugLevel) { logStream() << "[" << logTitle << "] [D] " << msg << "\n"; } };
 	#define LOCAL_ERROR(msg) logStream() << "[" << logTitle << "] " << "Error! " << msg << "\n"
 	#define LOCAL_WARNING(msg) logStream() << "[" << logTitle << "] " << "Warning: " << msg << "\n"
 	#define LOCAL_INFO(msg) logStream() << "[" << logTitle << "] " << msg << "\n"
@@ -20,7 +20,7 @@
 		#define LOG_VERBOSE(msg)
 	#endif
 	#define LOG(msg) std::cout << msg
-	#define LOG_DEBUG(msg) std::cout << msg
+	#define LOG_DEBUG(msg) std::cout << "[D] " << msg
 	#define LOG_ERROR(msg) std::cout << "Error! " << msg << "\n"
 	#define LOG_WARNING(msg) std::cout << "Warning: " << msg << "\n"
 	#define LOG_INFO(msg) std::cout << msg << "\n"
