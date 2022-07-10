5c1a77caf9109157ded1672a70a66b8e5d1eae5e
skalexey <skorohodov-ad@narod.ru>
2022-07-10 21:37:00 +0300

    Update Log.h: support LOG_LEVEL_DEBUG, LOG_LEVEL_VERBOSE

--- include/utils/Log.h
+++ include/utils/Log.h
@@ -30,23 +30,31 @@
 	// Is necessarily required for all LOCAL_LOG* functions
 	#define LOG_TITLE(title) namespace { const char* logTitle = title; } 
 	#define LOG_STREAM(s) namespace {logStream = s;}
-	#define SET_LOG_VERBOSE(boolVal) namespace { bool verboseLevel = boolVal;}
-	#define SET_LOG_DEBUG(boolVal) namespace { bool debugLevel = boolVal; }
 	#define LOCAL_LOG(msg) logStream() << "[" << logTitle << "] " << msg << "\n"
-	#define LOCAL_DEBUG(msg) { if (debugLevel) { logStream() << "[" << logTitle << "] [D] " << msg << "\n"; } };
+	#ifdef LOG_LEVEL_DEBUG
+		#define LOG_DEBUG(msg) logStream() << logPrefix << "[D] " << msg << logPostfix
+		#define LOCAL_DEBUG(msg) { if (debugLevel) { logStream() << "[" << logTitle << "] [D] " << msg << "\n"; } };
+		#define SET_LOG_DEBUG(boolVal) namespace { bool debugLevel = boolVal; }
+	#else
+		#define LOG_DEBUG(msg)
+		#define LOCAL_DEBUG(msg)
+		#define SET_LOG_DEBUG(boolVal)
+	#endif
 	#define LOCAL_ERROR(msg) logStream() << "[" << logTitle << "] " << "Error! " << msg << "\n"
 	#define LOCAL_WARNING(msg) logStream() << "[" << logTitle << "] " << "Warning: " << msg << "\n"
 	#define LOCAL_INFO(msg) logStream() << "[" << logTitle << "] " << msg << "\n"
-	#define LOCAL_VERBOSE(msg) { if (verboseLevel) { logStream() << "[" << logTitle << "] " << "[V] " << msg << "\n"; } };
 	#ifdef LOG_LEVEL_VERBOSE
 		#define LOG_VERBOSE(msg) std::cout << "[V] " << msg << "\n"
+		#define LOCAL_VERBOSE(msg) { if (verboseLevel) { logStream() << "[" << logTitle << "] " << "[V] " << msg << "\n"; } };
+		#define SET_LOG_VERBOSE(boolVal) namespace { bool verboseLevel = boolVal;}
 	#else
 		#define LOG_VERBOSE(msg)
+		#define LOCAL_VERBOSE(msg)
+		#define SET_LOG_VERBOSE(boolVal)
 	#endif
 	#define LOG_PREFIX(prefix) const auto prefixInitializer = PrefixInitializer(prefix)
 	#define LOG_POSTFIX(postfix) const auto postfixInitializer = PostfixInitializer(postfix)
 	#define LOG(msg) logStream() << logPrefix << msg << logPostfix
-	#define LOG_DEBUG(msg) logStream() << logPrefix << "[D] " << msg << logPostfix
 	#define LOG_ERROR(msg) logStream() << logPrefix << "Error! " << msg << (logPostfix.empty() ? "\n" : logPostfix)
 	#define LOG_WARNING(msg) logStream() << logPrefix << "Warning: " << msg << (logPostfix.empty() ? "\n" : logPostfix)
 	#define LOG_INFO(msg) logStream() << logPrefix << msg << (logPostfix.empty() ? "\n" : logPostfix)
