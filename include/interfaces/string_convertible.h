2434c3a4f33bccc5196c5d4149381a0d6e8c616b
skalexey <skorohodov-ad@narod.ru>
2022-07-13 12:28:02 +0300

    Added interfaces directory and string_convertible interface.

---
+++ include/interfaces/string_convertible.h
@@ -0,0 +1,20 @@
+﻿// string_convertible.h
+
+#pragma once
+
+#include <string>
+
+namespace interfaces
+{
+	class string_convertible
+	{
+	public:
+		virtual std::string to_string() const = 0;
+		inline operator std::string() const {
+			return to_string();
+		}
+	};
+
+	std::ostream& operator << (std::ostream& os, const string_convertible& p);
+}
+
---
+++ src/string_convertible.cpp
@@ -0,0 +1,17 @@
+﻿// string_convertible.cpp
+
+#include <ostream>
+#include <utils/Log.h>
+#include "interfaces/string_convertible.h"
+
+LOG_PREFIX("[string_convertible]: ");
+LOG_POSTFIX("\n");
+
+namespace interfaces
+{
+	std::ostream& operator << (std::ostream& os, const string_convertible& p)
+	{
+		os << p.to_string();
+		return os;
+	}
+}
