19657914480a617716f0c2e7daacc5657132bf8e
skalexey <skorohodov-ad@narod.ru>
2022-07-20 15:28:18 +0300

    str_toupper function added.

--- include/utils/string_utils.h
+++ include/utils/string_utils.h
@@ -9,6 +9,7 @@ namespace utils
 {
 	int count_substring(const std::string& where, const std::string& what);
 	std::string str_tolower(std::string s);
+	std::string str_toupper(std::string s);
 	std::string format_str(const char* fmt, ...);
 	std::vector<std::string_view> split(const std::string_view& str, const std::string_view& delim);
 	std::vector<std::string_view> split_of(const std::string_view& str, const std::string_view& delims);
--- src/string_utils.cpp
+++ src/string_utils.cpp
@@ -23,9 +23,18 @@ namespace utils
 		return cnt;
 	}
 
-	std::string str_tolower(std::string s) {
+	std::string str_tolower(std::string s)
+	{
 		std::transform(s.begin(), s.end(), s.begin(),
 			[](unsigned char c) { return std::tolower(c); } // correct
+		);
+		return s;
+	}
+
+	std::string str_toupper(std::string s)
+	{
+		std::transform(s.begin(), s.end(), s.begin(),
+			[](unsigned char c) { return std::toupper(c); } // correct
 		);
 		return s;
 	}
