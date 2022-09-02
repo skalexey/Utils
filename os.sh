3c0469559231e7b97a7fc116b203794b2fb7dccd
Alex <alexeiskorokhodov@gmail.com>
2022-09-02 20:30:01 +0300

    Update os.sh.

--- os.sh
+++ os.sh
@@ -2,20 +2,24 @@
 
 is_windows() {
 	if [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "win32" ]]; then
-		return 0
+		false
 	else
-		return 1
+        true
 	fi
 }
 
 is_nix() {
-	if [[ is_windows ]]; then
-		false
-	else
+	if is_windows; then
 		true
+	else
+		false
 	fi
 }
 
 is_mac() {
 	[[ $OSTYPE =~ .*darwin* ]] && true || false
+}
+
+is_linux() {
+    [[ $OSTYPE =~ .*linux* ]] && true || false
 }
