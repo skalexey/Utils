5dc5c00933a47bd046bc1139dbe66f082123d71a
skalexey <skorohodov-ad@narod.ru>
2023-03-19 07:36:06 +0100

    user_input::ask_line function for async UI-dialog-based asking a user for an input.

--- include/utils/ui/helpers/user_input.h
+++ include/utils/ui/helpers/user_input.h
@@ -17,14 +17,22 @@ namespace utils
 
 			void ask_user(
 				const std::string& question
-				, const std::function<void(bool)>& on_answer
+				, const utils::void_bool_cb& on_answer
 				, const char* yes_btn_text = nullptr
 				, const char* no_btn_text = nullptr
 			);
 
+			void ask_line(
+				const std::string& msg
+				, const utils::void_string_bool_cb& on_answer
+				, const std::string& default_value = ""
+				, const char* ok_btn_text = nullptr
+				, const char* cancel_btn_text = nullptr
+			);
+
 			void show_message(
 				const std::string& message
-				, const std::function<void()>& on_close = nullptr
+				, const utils::void_cb& on_close = nullptr
 				, const char* ok_btn_text = nullptr
 			);
 
--- src/ui/helpers/user_input.cpp
+++ src/ui/helpers/user_input.cpp
@@ -1,7 +1,14 @@
 #include <utils/ui/app.h>
 #include <utils/ui/imgui/widgets/dialogs/dialog_yes_no.h>
 #include <utils/ui/imgui/widgets/dialogs/dialog_message.h>
+#include <utils/ui/imgui/widgets/dialogs/dialog_input_text.h>
 #include <utils/ui/helpers/user_input.h>
+#include <utils/Log.h>
+#include <utils/common.h>
+#include <utils/string_utils.h>
+
+LOG_PREFIX("[user_input]: ");
+LOG_POSTFIX("\n");
 
 namespace utils
 {
@@ -9,11 +16,12 @@ namespace utils
 	{
 			void user_input::ask_user(
 				const std::string& question
-				, const std::function<void(bool)>& on_answer
+				, const utils::void_bool_cb& on_answer
 				, const char* yes_btn_text
 				, const char* no_btn_text
 			)
 			{
+				LOG("ask_user: '" << question << "' ['" << utils::to_str(yes_btn_text) << "'; '" << utils::to_str(no_btn_text) << "']\n");
 				auto d = std::make_shared<imgui::dialog_yes_no>(question, on_answer, yes_btn_text, no_btn_text, "Question");
 				m_app->add_on_update([=](float dt) {
 					return d->show();
@@ -22,14 +30,39 @@ namespace utils
 
 			void user_input::show_message(
 				const std::string& message
-				, const std::function<void()>& on_close
+				, const utils::void_cb& on_close
 				, const char* ok_btn_text
 			)
 			{
+				LOG("show_message: '" << message << "' ['" << utils::to_str(ok_btn_text) << "']\n");
 				auto d = std::make_shared<imgui::dialog_message>(message, [=](bool) {
 					if (on_close)
 						on_close();
 				}, ok_btn_text);
+				m_app->add_on_update([=](float dt) {
+					return d->show();
+				});
+			}
+
+			void user_input::ask_line(
+				const std::string& msg
+				, const utils::void_string_bool_cb& on_answer
+				, const std::string& default_value
+				, const char* ok_btn_text
+				, const char* cancel_btn_text
+			)
+			{
+				LOG("ask_line: '" << msg << "' ['" << utils::to_str(ok_btn_text) << "'; '" << utils::to_str(cancel_btn_text) << "']\n");
+				auto d = std::make_shared<utils::ui::imgui::dialog_input_text>(
+					msg
+					, [=](const std::string& path, bool cancelled) {
+						on_answer(path, cancelled);
+					}
+					, default_value
+					, ""
+					, ok_btn_text
+					, cancel_btn_text
+				);
 				m_app->add_on_update([=](float dt) {
 					return d->show();
 				});
