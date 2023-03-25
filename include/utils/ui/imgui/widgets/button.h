98d334a338cb6af9414e5f610583865d46675298
skalexey <skorohodov-ad@narod.ru>
2023-03-25 21:35:14 +0100

    ImGui button custom size support.

--- include/utils/ui/imgui/widgets/button.h
+++ include/utils/ui/imgui/widgets/button.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cmath>
 #include <memory>
 #include <imgui.h>
 #include <utils/ui/widgets/button.h>
@@ -19,14 +20,26 @@ namespace utils
 				button(const std::string& label, const on_click_t& on_click = nullptr) : base(label, on_click) {}
 
 				void on_show() override {
-					if (ImGui::Button(get_text().c_str()))
+					#ifdef ANDROID
+						auto ts = text_size();
+						vec2i new_size = {std::max(100, ts.x + 20), 100};
+						set_size(new_size);
+						const ImVec2 sz = {float(new_size.x), float(new_size.y)};
+					#else
+						const ImVec2 sz = {0, 0};
+					#endif
+					if (ImGui::Button(get_text().c_str(), sz))
 						on_click(true);
 					imgui::widget::on_show();
 				}
 
-				const vec2i& get_size() {
+				const vec2i& text_size() {
 					auto s = ImGui::CalcTextSize(get_text().c_str());
 					return m_calculated_size = { (int)s.x, (int)s.y };
+				}
+
+				const vec2i& get_size() {
+					return m_calculated_size;
 				}
 
 				private:
