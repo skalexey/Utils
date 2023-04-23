f8ffe0bdd96425803df9e391482b018958109597
skalexey <skorohodov-ad@narod.ru>
2023-04-23 17:53:16 +0200

    Store some experement results with abstract factories with variadic arguments.

---
+++ experimental/abstract_variadic_widget_factory.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <cstdarg>
+#include <memory>
+#include <typeinfo>
+#include <unordered_map>
+#include <functional>
+#include <utils/ui/fwd.h>
+
+namespace utils
+{
+	namespace ui
+	{
+		class widget_factory
+		{
+		public:
+			// TODO: use variadic arguments list
+			virtual text_ptr create_text() const { return nullptr; };
+			virtual label_ptr create_label() const { return nullptr; };
+			virtual button_ptr create_button() const { return nullptr; };
+			virtual text_input_ptr create_text_input() const { return nullptr; };
+			
+			//TODO: support variadic arguments
+			//template <typename T, typename... TArgs>
+			//std::shared_ptr<T> create(TArgs... args) const {
+			template <typename T, typename... Args>
+			std::shared_ptr<T> create(Args&&... args) const {
+				auto& creators = get_creators();
+				auto it = creators.find(typeid(T).name());
+				if (it != creators.end())
+				{
+					// return std::dynamic_pointer_cast<T>(it->second());
+					return std::dynamic_pointer_cast<T>((*it->second)());
+				}
+				return nullptr;
+			}
+
+		protected:
+
+			template <typename R>
+			struct creator_base
+			{
+				virtual std::shared_ptr<R> operator()() const = 0;
+			};
+
+			template <typename T, typename R>
+			struct creator_variadic : public creator_base<R>
+			{
+				std::shared_ptr<R> operator()() const override {
+					return std::dynamic_pointer_cast<R>(std::make_shared<T>());
+				}
+			};
+				//std::function<widget_ptr()>;
+			// using creator_t = std::function<widget_ptr()>;
+			using creator_t = creator_base<widget>;
+			using creator_t_ptr = std::shared_ptr<creator_t>;
+			using creators_t = std::unordered_map<std::string, creator_t_ptr>;
+			virtual creators_t& get_creators() const = 0;
+		};
+	}
+}
---
+++ experimental/concrete_variadic_widget_factory.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <unordered_map>
+#include <functional>
+#include <memory>
+#include <utils/ui/widget_factory.h>
+
+namespace utils
+{
+	namespace ui
+	{
+		namespace imgui
+		{
+			class widget_factory : public utils::ui::widget_factory
+			{
+			public:
+				static widget_factory& instance();
+				// TODO: use variadic arguments list
+				virtual utils::ui::text_ptr create_text() const;
+				virtual utils::ui::label_ptr create_label() const;
+				virtual utils::ui::button_ptr create_button() const;
+				virtual utils::ui::text_input_ptr create_text_input() const;
+
+			public:
+				template <typename Base, typename Final>
+				static void register_creator() {
+					if (!m_creators)
+						m_creators = new creators_t();
+					auto p = std::make_shared<creator_variadic<Final, ui::widget>>();
+					(*m_creators)[typeid(Base).name()] = std::dynamic_pointer_cast<creator_t>(p);
+				}
+
+			protected:
+				creators_t& get_creators() const override {
+					return *m_creators;
+				}
+			private:
+				static creators_t* m_creators;
+			};
+		}
+	}
+}
