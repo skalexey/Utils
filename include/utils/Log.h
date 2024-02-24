3017cd3827816fa4d5b375255e2e7bc15b06695a
skalexey <skorohodov-ad@narod.ru>
2024-02-24 10:04:16 +0100

    Tidy up

--- include/utils/Log.h
+++ include/utils/Log.h
@@ -9,58 +9,65 @@
 	{
 		using log_stream = std::conditional<is_complete<utils::user_log_stream>::value, utils::user_log_stream, utils::default_log_stream>::type;
 
-		std::string logPrefix; // Prefix for LOG_* commands. Is set through LOG_PREFIX function
-		std::string logPostfix = "\n"; // Postfix for LOG_* commands. Is set through LOG_POSTFIX function
+		std::string log_prefix; // Prefix for LOG_* commands. Is set through LOG_PREFIX function
+		std::string log_postfix = "\n"; // Postfix for LOG_* commands. Is set through LOG_POSTFIX function
 
-		struct PrefixInitializer
+		enum log_level_t : int
 		{
-			PrefixInitializer(const std::string& s) {
-				logPrefix = s;
+			log_level_error,
+			log_level_warning,
+			log_level_info,
+			log_level_debug,
+			log_level_verbose,
+			log_level_all
+		};
+		struct prefix_initializer
+		{
+			prefix_initializer(const std::string& s) {
+				log_prefix = s;
 			}
 		};
 
-		struct PostfixInitializer
+		struct postfix_initializer
 		{
-			PostfixInitializer(const std::string& s) {
-				logPostfix = s;
+			postfix_initializer(const std::string& s) {
+				log_postfix = s;
 			}
 		};
 	}
 
-	// Is necessarily required for all LOCAL_LOG* functions
-	#define LOCAL_LOG(msg) log_stream() << logPrefix << msg << "\n"
+	#define LOG_PREFIX(prefix) const auto _prefix_initializer_ = prefix_initializer(prefix);
+	#define LOG_POSTFIX(postfix) const auto _postfix_initializer_ = postfix_initializer(postfix);
+	#define LOG_TITLE(prefix) const auto _prefix_initializer_ = prefix_initializer("["prefix"]: ");
+
+	#define LOUT(msg) log_stream() << msg
+	#define LOG(msg) LOUT(log_prefix << msg << log_postfix)
+	#define LOCAL_LOG(msg) { if (local_log_level >= 0) { LOG(msg); } }
+	#define SET_LOCAL_LOG_LEVEL(level) namespace { int local_log_level = log_level_##level; }
 	#ifdef LOG_LEVEL_DEBUG
-		#define LOG_DEBUG(msg) log_stream() << logPrefix << "[D] " << msg << logPostfix
-		#define LOCAL_DEBUG(msg) { if (debugLevel) { LOG_DEBUG(msg); } };
-		#define SET_LOCAL_LOG_DEBUG(boolVal) namespace { bool debugLevel = boolVal; }
+		#define LOG_DEBUG(msg) LOG("[D] " << msg)
+		#define LOCAL_DEBUG(msg) { if (local_log_level == log_level_debug) { LOG_DEBUG(msg); } };
 	#else
 		#define LOG_DEBUG(msg)
 		#define LOCAL_DEBUG(msg)
-		#define SET_LOCAL_LOG_DEBUG(boolVal)
 	#endif
-	#define LOCAL_ERROR(msg) log_stream() << logPrefix << "Error! " << msg << "\n"
-	#define LOCAL_WARNING(msg) log_stream() << logPrefix << "Warning: " << msg << "\n"
-	#define LOCAL_INFO(msg) log_stream() << logPrefix << msg << "\n"
 	#ifdef LOG_LEVEL_VERBOSE
-        #define LOG_VERBOSE(msg) log_stream() << logPrefix << "[V] " << msg << "\n"
-		#define LOCAL_VERBOSE(msg) { if (verboseLevel) { LOG_VERBOSE(msg); } };
-		#define SET_LOCAL_LOG_VERBOSE(boolVal) namespace { bool verboseLevel = boolVal;}
+		#define LOG_VERBOSE(msg) LOG("[V] " << msg)
+		#define LOCAL_VERBOSE(msg) { if (local_log_level == log_level_verbose) { LOG_VERBOSE(msg); } };
 	#else
 		#define LOG_VERBOSE(msg)
 		#define LOCAL_VERBOSE(msg)
-		#define SET_LOCAL_LOG_VERBOSE(boolVal)
 	#endif
-	#define LOG_PREFIX(prefix) const auto prefixInitializer = PrefixInitializer(prefix);
-	#define LOG_POSTFIX(postfix) const auto postfixInitializer = PostfixInitializer(postfix);
-	#define LOG_TITLE(prefix) const auto prefixInitializer = PrefixInitializer("["#prefix"]: ");
-	#define LOG(msg) log_stream() << logPrefix << msg << logPostfix
-	#define LOG_ERROR(msg) log_stream() << logPrefix << "Error! " << msg << (logPostfix.empty() ? "\n" : logPostfix)
-	#define LOG_WARNING(msg) log_stream() << logPrefix << "Warning: " << msg << (logPostfix.empty() ? "\n" : logPostfix)
-	#define LOG_INFO(msg) log_stream() << logPrefix << msg << (logPostfix.empty() ? "\n" : logPostfix)
+	#define LOG_ERROR(msg) LOG("[E] Error! " << msg)
+	#define LOG_WARNING(msg) LOG("[W] Warning: " << msg)
+	#define LOG_INFO(msg) LOG("[I] " << msg)
+	#define LOCAL_ERROR(msg) { if (local_log_level == log_level_error) { LOG_ERROR(msg); } }
+	#define LOCAL_WARNING(msg) { if (local_log_level == log_level_warning) { LOG_WARNING(msg); } }
+	#define LOCAL_INFO(msg) { if (local_log_level == log_level_info) { LOG_INFO(msg); } }
 	
 	// Print and invoke an expression
 	#define LOG_EXPR(ex) LOG(#ex); ex
-	#define LOG_EXPR_LOG(ex, msg) log_stream() << logPrefix << #ex << msg << logPostfix; ex
+	#define LOG_EXPR_LOG(ex, msg) LOG(#ex << msg); ex
 	#define LOG_EXPR_VAL(ex) LOG(ex)
 	#define LOG_EXPR_AND_VAL(ex) LOG(#ex << ": " << ex)
 	#define LOG_EXPR_AND_VAL_LOG(ex, msg) LOG(#ex << ": " << ex << msg)
@@ -68,8 +75,7 @@
 	#define LOG_PREFIX(prefix)
 	#define LOG_POSTFIX(postfix)
 	#define LOG_TITLE(title)
-	#define SET_LOCAL_LOG_VERBOSE(boolVal)
-	#define SET_LOCAL_LOG_DEBUG(boolVal)
+	#define SET_LOCAL_LOG_LEVEL(level)
 	#define LOCAL_LOG(msg)
 	#define LOCAL_DEBUG(msg)
 	#define LOCAL_WARNING(msg)
